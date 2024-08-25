6cc4131b58e5681b92faa767c7d5360953367b8e
jazztyy <jazztyy@gmail.com>
2024-08-25 18:01:20 +0800

    [Fix] astro respority base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://jazztyy.github.io',
-	base: '/',
+	base: '/blog',
 	integrations: [mdx(), sitemap()],
 });
